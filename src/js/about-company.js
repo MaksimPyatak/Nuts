f5eb557431a7522b873c719a94779e47122b8141
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-07-30 23:31:31 +0300

    added abuot-company page

---
+++ src/js/about-company.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+import "./modules/burger-menu.js";
+import "./modules/vieo-modal.js"
+import "./modules/about-manufacturer-swiper.js"
+import "./modules/news.js"
+
