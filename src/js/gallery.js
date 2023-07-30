3e93d59515d1285f56b83a87e360dfd54b72a115
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-07-30 23:45:52 +0300

    added gallery.js

---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+import "./modules/burger-menu.js";
+import "./modules/product-card.js"
+import "./modules/img-modal.js"
+import "./modules/about-manufacturer-swiper.js"
+import "./modules/select.js"
+import "./modules/scroll-container.js"
