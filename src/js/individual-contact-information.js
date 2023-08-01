5e1f184a8da6440ca76f0e79abbe260f5243b502
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-08-01 10:36:25 +0300

    aqdded photo display

--- src/js/individual-contact-information.js
+++ src/js/individual-contact-information.js
@@ -4,3 +4,4 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 import "./modules/burger-menu.js";
+import "./modules/input-file.js";
---
+++ src/js/modules/input-file.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('photo');
+
+if (input !== null) {
+   const labelElement = document.querySelector('label[for="photo"]');
+   const imgElement = labelElement.querySelector('img');
+   const labelContainer = labelElement.querySelector('div');
+   const labelText = labelContainer.querySelector('div');
+   input.addEventListener('change', updatePhoto)
+   function updatePhoto() {
+      if (input.files.length > 0) {
+         imgElement.src = URL.createObjectURL(input.files[0]);
+         labelContainer.style.background = 'rgba(0, 0, 0, 0.30)';
+         labelText.textContent = 'Изменить фото'
+      } else {
+         imgElement.src = 'img/account/empty-photo.svg';
+         labelContainer.style.background = '#337D5A';
+         labelText.textContent = 'Загрузить фото'
+      }
+   }
+}
+console.log(input.files.length);
